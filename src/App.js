eaf7875819cefbc6645dc60f42e72b015634c46f
unknown <rajadityaraj03@gmail.com>
2025-07-23 17:22:30 +0530

    2nd recommit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactttttttttttttt
+          Learnnnn React
         </a>
       </header>
     </div>
